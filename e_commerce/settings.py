f71a8172566caca01f439882550974ea6edce9ea
unknown <phaferro@hotmail.com>
2023-02-01 14:54:32 -0300

    insert static root

--- e_commerce/settings.py
+++ e_commerce/settings.py
@@ -122,7 +122,9 @@ STATICFILES_DIRS = [
      os.path.join(BASE_DIR, "static_local")
 ]
 
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn", "static_root")
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+#DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
